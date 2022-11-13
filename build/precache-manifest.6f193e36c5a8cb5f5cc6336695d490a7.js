77abf7140cfdc7823586bc13dc561432cc3e4923
Benjamin Marks <bjmarks13@yahoo.com>
2022-11-12 19:36:37 -0600

    deploy

---
+++ build/precache-manifest.6f193e36c5a8cb5f5cc6336695d490a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe1c0d85cc44a0941fb9",
+    "url": "/static/css/main.38d4d871.chunk.css"
+  },
+  {
+    "revision": "fe1c0d85cc44a0941fb9",
+    "url": "/static/js/main.fe1c0d85.chunk.js"
+  },
+  {
+    "revision": "f12f5854ce980c3a420c",
+    "url": "/static/js/1.f12f5854.chunk.js"
+  },
+  {
+    "revision": "5013d5d81d8117598d60",
+    "url": "/static/js/2.5013d5d8.chunk.js"
+  },
+  {
+    "revision": "86af42aa9a6f3e84c441",
+    "url": "/static/js/runtime~main.86af42aa.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "6693e82ea9b41cb7e4618daec2bf4b99",
+    "url": "/index.html"
+  }
+];
