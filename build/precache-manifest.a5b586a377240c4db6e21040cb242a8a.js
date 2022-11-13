cda8e78c960c974cf9ccd793175d432b11e248cb
Ryan Yoshimura <ryan.s.yoshimura@gmail.com>
2022-11-12 18:56:53 -0600

    update

---
+++ build/precache-manifest.a5b586a377240c4db6e21040cb242a8a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2603de26072932369b04",
+    "url": "/static/css/main.6c34394c.chunk.css"
+  },
+  {
+    "revision": "2603de26072932369b04",
+    "url": "/static/js/main.2603de26.chunk.js"
+  },
+  {
+    "revision": "f12f5854ce980c3a420c",
+    "url": "/static/js/1.f12f5854.chunk.js"
+  },
+  {
+    "revision": "ceac132f2d6abdc7b793",
+    "url": "/static/js/2.ceac132f.chunk.js"
+  },
+  {
+    "revision": "86af42aa9a6f3e84c441",
+    "url": "/static/js/runtime~main.86af42aa.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "3ab34a9053a3bd1173b122b990f8010f",
+    "url": "/index.html"
+  }
+];
