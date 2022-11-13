cc200fc6431f981c30a6d138d03b6c87be4e948a
Ryan Yoshimura <ryan.s.yoshimura@gmail.com>
2022-11-12 21:48:01 -0600

    placeholders for projects in progress...

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -26,12 +26,149 @@ export class Home extends Component{
                     <div className="mainContainer">
                         {/*<div><img className="reactLogo" src={logo} alt="img"></img></div>*/}
                         {/*<div><img className="reactLogo2"src="https://files.virgool.io/upload/users/24829/posts/m0a581jdk8zj/30ovzh9b1xr0.jpeg" alt="img"></img></div>*/}
-                        <div>Things here</div>
+                        <div id="mainProjectContainer">
+                            <div className="projects">
+                                <div>projects here</div>
+                            </div>
+                            <div className="projects">
+                                <div>projects here</div>
+                            </div>
+                            <div className="projects">
+                                <div>projects here</div>
+                            </div>
+                            <div className="projects">
+                                <div>projects here</div>
+                            </div>
+                            <section>
+
+                                <div className="img_grid_all" id="img_grids">
+
+                                    <div className="img_grind_mask_wrap">
+                                        <div className="img_grind_mask_body">
+                                            {/*<a href="https://www.letters-inc.jp/works/#amp"*/}
+                                            {/*   className="img_grind_tgt img_grind_tgt1" data-tgt="img_grid_body1">AMP*/}
+                                            {/*    MUSIC</a>*/}
+                                            {/*<a href="https://www.letters-inc.jp/works/#niky"*/}
+                                            {/*   className="img_grind_tgt img_grind_tgt2" data-tgt="img_grid_body2">NIKY*/}
+                                            {/*    ROEHREKE</a>*/}
+                                            {/*<a href="https://www.letters-inc.jp/works/#midori"*/}
+                                            {/*   className="img_grind_tgt img_grind_tgt3" data-tgt="img_grid_body3">MIDORI*/}
+                                            {/*    AOYAMA</a>*/}
+                                            {/*<a href="https://www.letters-inc.jp/works/#mugen"*/}
+                                            {/*   className="img_grind_tgt img_grind_tgt4"*/}
+                                            {/*   data-tgt="img_grid_body4">MUGENUP</a>*/}
+                                        </div>
+                                    </div>
+
+                                    <div className="img_grid_inner">
+                                        <div className="img_grid_wrap">
+
+                                            <div className="img_grid_body img_grid_body1">
+                                                <div className="img_grid">
+                                                    <svg width="982.581px" height="539.545px">
+                                                        <path className="b"
+                                                              d="M39.286,295.771c0,0,224.106-104.501,271.103-126.416 S626.287,22.051,626.287,22.051l314.939,220.522L354.235,516.295L39.286,295.771z"></path>
+                                                        <path className="p" d="M0.287,268.389l386.56,270.747"></path>
+                                                        <path className="p" d="M673.101,0.953L0.786,314.457"></path>
+                                                        <path className="p" d="M307.387,538.136L982.37,223.388"></path>
+                                                        <path className="p" d="M980.869,270.681L597.476,2.226"></path>
+                                                        <polygon
+                                                            points="41.774,297.513 626.287,24.549 938.404,243.89 354.226,516.295 "></polygon>
+                                                        <polygon fill="none"
+                                                                 points="621.143,24.448 337.5,156.712 621.143,24.449 "></polygon>
+                                                        <polygon fill="#222"
+                                                                 points="805.088,147.248 631.43,25.651 626.287,22.051 626.286,22.05 621.143,24.449 337.5,156.712 39.286,295.771 44.429,299.372 626.286,28.05 936.083,244.972 941.227,242.573 "></polygon>
+                                                    </svg>
+                                                    <span className="bdL"></span>
+                                                    <span className="bdR"></span>
+                                                    <img
+                                                        // src="https://www.letters-inc.jp/wp-content/themes/g/img/g4_blue.png"
+                                                        className="img_dark" alt=""/>
+                                                </div>
+                                            </div>
+
+                                            <div className="img_grid_body img_grid_body2">
+                                                <div className="img_grid">
+                                                    <svg width="982.581px" height="539.545px">
+                                                        <path className="b"
+                                                              d="M39.286,295.771c0,0,224.106-104.501,271.103-126.416 S626.287,22.051,626.287,22.051l314.939,220.522L354.235,516.295L39.286,295.771z"></path>
+                                                        <path className="p" d="M0.287,268.389l386.56,270.747"></path>
+                                                        <path className="p" d="M673.101,0.953L0.786,314.457"></path>
+                                                        <path className="p" d="M307.387,538.136L982.37,223.388"></path>
+                                                        <path className="p" d="M980.869,270.681L597.476,2.226"></path>
+                                                        <polygon
+                                                            points="41.774,297.513 626.287,24.549 938.404,243.89 354.226,516.295 "></polygon>
+                                                        <polygon fill="none"
+                                                                 points="621.143,24.448 337.5,156.712 621.143,24.449 "></polygon>
+                                                        <polygon fill="#222"
+                                                                 points="805.088,147.248 631.43,25.651 626.287,22.051 626.286,22.05 621.143,24.449 337.5,156.712 39.286,295.771 44.429,299.372 626.286,28.05 936.083,244.972 941.227,242.573 "></polygon>
+                                                    </svg>
+                                                    <span className="bdL"></span>
+                                                    <span className="bdR"></span>
+                                                    <img
+                                                        // src="https://www.letters-inc.jp/wp-content/themes/g/img/g2_blue.png"
+                                                        className="img_dark" alt=""/>
+                                                </div>
+                                            </div>
+
+                                            <div className="img_grid_body img_grid_body3">
+                                                <div className="img_grid">
+                                                    <svg width="982.581px" height="539.545px">
+                                                        <path className="b"
+                                                              d="M39.286,295.771c0,0,224.106-104.501,271.103-126.416 S626.287,22.051,626.287,22.051l314.939,220.522L354.235,516.295L39.286,295.771z"></path>
+                                                        <path className="p" d="M0.287,268.389l386.56,270.747"></path>
+                                                        <path className="p" d="M673.101,0.953L0.786,314.457"></path>
+                                                        <path className="p" d="M307.387,538.136L982.37,223.388"></path>
+                                                        <path className="p" d="M980.869,270.681L597.476,2.226"></path>
+                                                        <polygon
+                                                            points="41.774,297.513 626.287,24.549 938.404,243.89 354.226,516.295 "></polygon>
+                                                        <polygon fill="none"
+                                                                 points="621.143,24.448 337.5,156.712 621.143,24.449 "></polygon>
+                                                        <polygon fill="#222"
+                                                                 points="805.088,147.248 631.43,25.651 626.287,22.051 626.286,22.05 621.143,24.449 337.5,156.712 39.286,295.771 44.429,299.372 626.286,28.05 936.083,244.972 941.227,242.573 "></polygon>
+                                                    </svg>
+                                                    <span className="bdL"></span>
+                                                    <span className="bdR"></span>
+                                                    <img
+                                                        // src="https://www.letters-inc.jp/wp-content/themes/g/img/g1_blue.png"
+                                                        className="img_dark" alt=""/>
+                                                </div>
+                                            </div>
+
+                                            <div className="img_grid_body img_grid_body4">
+                                                <div className="img_grid">
+                                                    <svg width="982.581px" height="539.545px">
+                                                        <path className="b"
+                                                              d="M39.286,295.771c0,0,224.106-104.501,271.103-126.416 S626.287,22.051,626.287,22.051l314.939,220.522L354.235,516.295L39.286,295.771z"></path>
+                                                        <path className="p" d="M0.287,268.389l386.56,270.747"></path>
+                                                        <path className="p" d="M673.101,0.953L0.786,314.457"></path>
+                                                        <path className="p" d="M307.387,538.136L982.37,223.388"></path>
+                                                        <path className="p" d="M980.869,270.681L597.476,2.226"></path>
+                                                        <polygon
+                                                            points="41.774,297.513 626.287,24.549 938.404,243.89 354.226,516.295 "></polygon>
+                                                        <polygon fill="none"
+                                                                 points="621.143,24.448 337.5,156.712 621.143,24.449 "></polygon>
+                                                        <polygon fill="#222"
+                                                                 points="805.088,147.248 631.43,25.651 626.287,22.051 626.286,22.05 621.143,24.449 337.5,156.712 39.286,295.771 44.429,299.372 626.286,28.05 936.083,244.972 941.227,242.573 "></polygon>
+                                                    </svg>
+                                                    <span className="bdL"></span>
+                                                    <span className="bdR"></span>
+                                                    <img
+                                                        // src="https://www.letters-inc.jp/wp-content/themes/g/img/g3_blue.png"
+                                                        className="img_dark" alt=""/>
+                                                </div>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </section>
+                        </div>
+
+
+
                     </div>
-
-
-
-
                 </header>
             </div>
         );
