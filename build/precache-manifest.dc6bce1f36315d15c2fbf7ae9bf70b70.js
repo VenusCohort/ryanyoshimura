45c390a8dfad965af63e79ea1e43f29cdfcec4f3
Benjamin Marks <bjmarks13@yahoo.com>
2022-11-12 18:48:38 -0600

    deploy 2

---
+++ build/precache-manifest.dc6bce1f36315d15c2fbf7ae9bf70b70.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "330fe1b6baeca24dbea2",
+    "url": "/static/css/main.61e3c558.chunk.css"
+  },
+  {
+    "revision": "330fe1b6baeca24dbea2",
+    "url": "/static/js/main.330fe1b6.chunk.js"
+  },
+  {
+    "revision": "f12f5854ce980c3a420c",
+    "url": "/static/js/1.f12f5854.chunk.js"
+  },
+  {
+    "revision": "1f7db0e2505cec9949dd",
+    "url": "/static/js/2.1f7db0e2.chunk.js"
+  },
+  {
+    "revision": "86af42aa9a6f3e84c441",
+    "url": "/static/js/runtime~main.86af42aa.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "2f6653faece8b295bff031eed41ba708",
+    "url": "/index.html"
+  }
+];
