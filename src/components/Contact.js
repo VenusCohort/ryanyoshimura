1f7ef6830c4a68789b44fa8b90e731ba3f569526
Ryan Yoshimura <ryan.s.yoshimura@gmail.com>
2022-12-10 00:25:36 -0600

    added flexbox sizing to email form/styled bg

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,44 +10,45 @@ export class Contact extends Component{
             // TODO: need to target button using virtual DOM for a sweet css effect.
         }
         return (
-            <main id="main-container">
-                <div id="infoContainer">
-                    {/*<div id="container-etc"></div>*/}
-                    <form method="post">
-                        <img id="yoshi-img" src={yoshiProfilePic} alt="img" />
-                        <br/><br/>
-                        <span>
-                            Hi There! Send me an email and I will get right back to you. <br/><div>Mahalo!</div>
-                        </span>
-                        <br/><br/>
-                        <div id="email-container">
-                            <span>
-                            <label htmlFor="name">Name </label>
-                            <input type="text" name="name"/>
-                        </span>
+            <div id="contact-background">
+                <main id="main-container">
+                    <div id="infoContainer">
+                        {/*<div id="container-etc"></div>*/}
+                        <form method="post">
+                            <img id="yoshi-img" src={yoshiProfilePic} alt="img" />
                             <br/><br/>
                             <span>
-                            <label htmlFor="email">Email &nbsp;</label>
-                            <input type="text" name="email"/>
-                        </span>
+                                Hi There! Send me an email and I will get right back to you. <br/><div>Mahalo!</div>
+                            </span>
                             <br/><br/>
-                            <span>
-                            <label htmlFor="message">Message &nbsp;</label>
-                            <textarea name="message"/>
-                        </span>
-                            <br/><br/>
-                            <span id="submit-button-wrapper">
-                            <button onClick={btnAction}>
-                                Submit
-                            </button>
-                        </span>
-                        </div>
+                            <div id="email-container">
+                                <span>
+                                <label htmlFor="name">Name </label>
+                                <input type="text" name="name"/>
+                            </span>
+                                <br/><br/>
+                                <span>
+                                <label htmlFor="email">Email &nbsp;</label>
+                                <input type="text" name="email"/>
+                            </span>
+                                <br/><br/>
+                                <span>
+                                <label htmlFor="message">Message &nbsp;</label>
+                                <textarea name="message"/>
+                            </span>
+                                <br/><br/>
+                                <span id="submit-button-wrapper">
+                                <button onClick={btnAction}>
+                                    Submit
+                                </button>
+                            </span>
+                            </div>
 
 
-                    </form>
-                </div>
-            </main>
-
+                        </form>
+                    </div>
+                </main>
+            </div>
     )
     }
 }
